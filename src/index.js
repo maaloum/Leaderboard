287754e3f9153dea7476a90ef292c0e5de0d3fda
Maaloum <50062179+maaloum@users.noreply.github.com>
2022-09-01 11:24:37 +0300

    Hit the API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
 import './style.css';
+
+// GameID : EEOK6HDsrV9GBtJdvBFP
+const tableData = document.querySelector('.tableData');
+const userName = document.querySelector('.user');
+const userScore = document.querySelector('.score');
+const refresh = document.querySelector('.refresh');
+const btnSubmit = document.querySelector('.btnSubmit');
+const gameUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/EEOK6HDsrV9GBtJdvBFP/scores/';
+
+const getScores = async () => {
+  const response = await fetch(gameUrl);
+  const data = await response.json();
+  return data.result;
+};
+
+const postScore = async () => {
+  const response = await fetch(gameUrl, {
+    method: 'POST',
+    body: JSON.stringify({ user: userName.value, score: userScore.value }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const data = await response.json();
+  return data.result;
+};
+
+const refreshScores = async () => {
+  const scores = await getScores();
+  tableData.innerHTML = '';
+  scores.forEach((score) => {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+      <td>${score.user}</td>
+      <td>${score.score}</td>
+    `;
+    tableData.appendChild(row);
+  });
+};
+
+refresh.addEventListener('click', refreshScores);
+
+btnSubmit.addEventListener('click', (e) => {
+  e.preventDefault();
+  postScore(userName, userScore);
+  refreshScores();
+  userName.value = '';
+  userScore.value = '';
+});
